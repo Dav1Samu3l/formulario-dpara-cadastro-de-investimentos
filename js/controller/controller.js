1efc853d9acb36b6c233dfd02582215d4880ed91
Dav1Samu3l <davidev.front@gmail.com>
2023-02-27 14:28:23 -0300

    fim

--- js/controller/controller.js
+++ js/controller/controller.js
@@ -13,37 +13,33 @@ export class NegociacaoController {
 
     criarNegociacao() {
         
-        let data = new Date(this._campoData.value + "T00:00:00-03:00");
+        let data = new Date(this._campoData.value + "T00:00:00-03:00").toLocaleDateString();
         let quantidade = parseInt(this._quantidadeAcoes.value);
         let valor = parseFloat(this.campoValor.value);
         var novaNegociacao = new Negociacao(data, quantidade, valor);
         console.log(novaNegociacao);
 
-
-        // Seleciona as células da tabela
-        const celulaData = document.querySelector('#data');
-        const celulaQuantidade = document.querySelector('#quantidade');
-        const celulaValor = document.querySelector('#valor');
-
-        // Atualiza o conteúdo das células com os valores dos inputs
-        celulaData.innerHTML = (novaNegociacao.date.toLocaleDateString());
-        celulaQuantidade.innerHTML = novaNegociacao.quantidade;
-        celulaValor.innerHTML = novaNegociacao.valor;
-
+        
+        
         // Cria uma nova linha na tabela
         const novaLinha = document.createElement('tr');
-
+        
         // Cria uma nova celula na tabela e insri o valor da data
         const novaColuna = document.createElement('td');
-        novaColuna.innerHTML = novaNegociacao.date.toLocaleDateString();
+        novaColuna.innerHTML = novaNegociacao.date
+        const celulaData = document.querySelector('#data');
         
         // Cria uma nova celula na tabela e inseri a quantidade
         const celNovaNegociacao = document.createElement('td');
         celNovaNegociacao.innerHTML = novaNegociacao.quantidade;
+        const celulaQuantidade = document.querySelector('#quantidade');
+        
         
         // Cria uma nova celula na tabela e inserio valor 
         const celulaVal = document.createElement('td');
         celulaVal.innerHTML = novaNegociacao.valor;
+        const celulaValor = document.querySelector('#valor');
+
 
         // Adiciona as células à nova linha
         novaLinha.appendChild(novaColuna);
